c4ed3ec3b33b832da889370540ec37c0f969ab2a
gayathri-kayapati <gayathrikayapati@gmail.com>
2023-06-06 23:33:16 +0530

    feat:added functionality for wishlist icon

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+
+import { WishlistContext } from "../../Contexts/WishlistContextProvider";
 import "./productCard.css";
 import { AiOutlineHeart } from "react-icons/ai";
+import { AiFillHeart } from "react-icons/ai";
 
 export default function ProductCard({ product }) {
+  const { wishlist, handleWishlistItems, handleRemoveWishlistItems } =
+    useContext(WishlistContext);
+
   const {
     id,
     image,
@@ -11,11 +18,32 @@ export default function ProductCard({ product }) {
     discountPrice,
     discountPercentage,
   } = product;
+
+  const isAlreadyInWishlist = wishlist.find((item) => item.id === id);
+
   return (
     <div key={id} className="productCard">
-      <div className="circle">
-        <AiOutlineHeart />
-      </div>
+      {isAlreadyInWishlist ? (
+        <div className="circle">
+          <AiFillHeart
+            style={{ color: "red" }}
+            onClick={(e) => {
+              e.stopPropagation();
+              handleRemoveWishlistItems(product);
+            }}
+          />
+        </div>
+      ) : (
+        <div className="circle">
+          <AiOutlineHeart
+            onClick={(e) => {
+              e.stopPropagation();
+              handleWishlistItems(product);
+            }}
+          />
+        </div>
+      )}
+
       <img src={image} alt={title} className="productImage" />
       <div className="productDescription">
         <h3 className="title">{title}</h3>
